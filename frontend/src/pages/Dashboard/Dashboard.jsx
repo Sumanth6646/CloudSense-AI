071bbffb1d2e78d36c279407ee09c456fbcf1a5e
SaiSumanthPonna <saisumanthponna@gmail.com>
2026-07-29 21:02:52 +0530

    updated readme

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,24 @@
+function Dashboard() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#F8FAFC",
+        minHeight: "100vh",
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>☁ CloudSense AI</h1>
+
+      <h2>Welcome, Sai 👋</h2>
+
+      <p>AI-Powered Cloud Cost Dashboard & Anomaly Detection System</p>
+
+      <hr />
+
+      <h3>Dashboard Coming Soon 🚀</h3>
+    </div>
+  );
+}
+
+export default Dashboard;
